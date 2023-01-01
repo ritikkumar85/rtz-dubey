431f694df30b9b0d2db4e50b7494bd61b0862349
ritikkumar85 <ritikkumar1793@gmail.com>
2023-01-01 20:12:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// document.querySelector(".logo").innerHTML="change";
+// document.addEventListener("click",func);
+// function func(){
+//     document.getElementById("ho").innerHTML=" the tewxt is changed"
+// }
